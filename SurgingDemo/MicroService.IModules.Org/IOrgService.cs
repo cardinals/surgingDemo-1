f84a303f960bb4894cf05199e8d17cd64d1e3336
NaturalDragon <501159436@qq.com>
2019-01-15 11:28:38 +0800

    add orgServer

---
+++ SurgingDemo/MicroService.IModules.Org/IOrgService.cs
@@ -0,0 +1,15 @@
+﻿using Surging.Core.CPlatform.Ioc;
+using Surging.Core.CPlatform.Runtime.Server.Implementation.ServiceDiscovery.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroService.IModules.Org
+{
+    [ServiceBundle("api/{Service}")]
+    public interface IOrgService: IServiceKey
+    {
+
+        int Number(int x, int y);
+    }
+}
---
+++ SurgingDemo/MicroService.Modules.Org/OrgService.cs
@@ -0,0 +1,18 @@
+﻿using MicroService.IModules.Org;
+using Surging.Core.CPlatform.Ioc;
+using Surging.Core.ProxyGenerator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroService.Modules.Org
+{
+    [ModuleName("Org")]
+    public class OrgService : ProxyServiceBase, IOrgService
+    {
+        public int Number(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
